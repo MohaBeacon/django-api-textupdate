c652d575d940b714da1cf58990c01c3086be94ef
MohaBeacon <mohanvjm@outlook.com>
2024-12-18 05:36:29 +0530

    update host

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&#@#g9#817jpn+b^*dqaj@ad=255^evl4q#frw2_@8akqrda36
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
